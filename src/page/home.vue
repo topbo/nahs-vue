43377ea7688c1dc100be568477018a991888adcc
xiebo0707 <1141968148@qq.com>
2017-07-26 17:37:36 +0800

    一些侧边栏和走马灯

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <HeaderBar></HeaderBar>
+    <SideBar></SideBar>
+    <el-carousel height="380px">
+      <el-carousel-item v-for="item in 7" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 import HeaderBar from '../components/HeaderBar'
+import SideBar from '../components/SideBar'
 export default {
 
   name: 'home',
@@ -16,10 +23,26 @@ export default {
     }
   },
   components: {
-    HeaderBar
+    HeaderBar,
+    SideBar
   }
 }
 </script>
 
 <style lang="css" scoped>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
 </style>
